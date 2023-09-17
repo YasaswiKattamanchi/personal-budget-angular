44599485288f6292b9fd8ffd2de45baf14c2e495
yasaswikattamanchi <yasaswikattamanchi@gmail.com>
2023-09-17 17:14:34 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,38 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    "myBudget": [
-      {
-        "title": "Rent",
-        "budget": 10000
-      },
-     {
-        "title": "Gas",
-        "budget": 22455
-      },
-     {
-        "title": "Wifi",
-        "budget": 375
-      },
-     {
-        "title": "expenses",
-        "budget": 375
-      },
-     
-    
-    ]
-  };
+const data = fs.readFileSync('budget.json', 'utf8');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget',(req, res) => {
-    res.json(budget);
+    res.send(data);
 });
 
 app.listen(port, () => {
